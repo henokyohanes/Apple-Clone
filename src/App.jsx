b1b1b4dcf9dc0be4f625f9cfbdf13b30e79815a7
henokyohanesdev <henokyohanes8@gmail.com>
2024-10-27 05:39:17 -0500

    header component with fully responsive styling implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import './App.css'
+import "./assets/css/bootstrap.css";
+import "./assets/css/styles.css";
+import Header from './Components/Header'
 
 const App = () => {
   return (
     <>
-      
+      <Header />
     </>
   )
 }
