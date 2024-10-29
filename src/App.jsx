a246d4072ecd3c80228cd368a3a28d786e281947
henokyohanesdev <henokyohanes8@gmail.com>
2024-10-29 00:20:23 -0500

    section component and responsive styling implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./assets/css/bootstrap.css";
 import "./assets/css/styles.css";
 import Header from './Components/Header'
+import Section from './Components/Section';
 import Footer from './Components/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Section />
       <Footer />
     </>
   )
