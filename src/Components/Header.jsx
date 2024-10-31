fe68e54e574c2d38980bc6000941a35106d9e84f
henokyohanesdev <henokyohanes8@gmail.com>
2024-10-30 20:50:34 -0500

    code cleaning done and project deployed.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,37 +3,61 @@ import appleLogo from '../assets/images/icons/logo-sm.png'
 import searchIcon from '../assets/images/icons/search-icon-sm.png'
 import cart from '../assets/images/icons/cart-sm.png'
 
-
 function Header() {
+
+    // Render the header
     return (
-        <>
-            <div className="nav-wrapper fixed-top">
-                <div className="container">
-                    <nav className="navbar navbar-toggleable-sm navbar-expand-md">
-                        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                            data-target=".navbar-collapse">
-                            ☰
-                        </button>
-                        <a className="navbar-brand mx-auto" href="#"><img src={appleLogo} /></a>
-                        <div className="navbar-collapse collapse">
-                            <ul className="navbar-nav nav-justified w-100 nav-fill">
-                                <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">Mac</a></li>
-                                <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">iphone</a></li>
-                                <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">ipad</a></li>
-                                <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">watch</a></li>
-                                <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">tv</a></li>
-                                <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">Music</a></li>
-                                <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">Support</a></li>
-                                <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#"><img
-                                    src={searchIcon} /></a></li>
-                                <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#"><img
-                                    src={cart} /></a></li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
+        // Header container
+        <div className="nav-wrapper fixed-top">
+            <div className="container">
+                <nav className="navbar navbar-toggleable-sm navbar-expand-md">
+
+                    {/* Navbar toggler button */}
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                        data-target=".navbar-collapse">
+                        ☰
+                    </button>
+
+                    {/* Navbar brand logo*/}
+                    <a className="navbar-brand mx-auto" href="#"><img src={appleLogo} /></a>
+
+                    {/* Navbar collapse menu items*/}
+                    <div className="navbar-collapse collapse">
+                        <ul className="navbar-nav nav-justified w-100 nav-fill">
+
+                            {/* Navbar menu items */}
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#">Mac</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#">iphone</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#">ipad</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#">watch</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#">tv</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#">Music</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#">Support</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#"><img src={searchIcon} /></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#"><img src={cart} /></a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
-        </>
+        </div>
     )
 }
 
--- src/Components/Section.jsx
+++ src/Components/Section.jsx
@@ -6,8 +6,11 @@ import arcade from '../assets/images/icons/arcade.png'
 
 const Section = () => {
 
+    // Render the section component
     return (
         <>
+
+            {/* Alert section  */}
             <section className="alert-section top-50">
                 <div className="container">
                     <div className="alert-title">
@@ -16,59 +19,37 @@ const Section = () => {
                     <div className="alert-text">
                         Our retail stores are closed, but you can buy our products here online and get fast, free delivery. If
                         you need help finding the right product or have a question on your order, chat online with a Specialist
-                        or call 1-800-MY-APPLE.<br/>
-                            For service and support, visit <a href="https://support.apple.com/">support.apple.com</a>.
+                        or call 1-800-MY-APPLE.<br />
+                        For service and support, visit <a href="https://support.apple.com/">support.apple.com</a>.
                     </div>
                 </div>
             </section>
 
-            {/* First section starts here  */}
+            {/* First section  */}
             <section className="first-hightlight-wrapper">
                 <div className="container">
-
-                    <div className="new-alert">
-                        New
-                    </div>
-
-                    <div className="title-wraper bold black">
-                        iPad Pro
-                    </div>
-
+                    <div className="new-alert">New</div>
+                    <div className="title-wraper bold black">iPad Pro</div>
                     <div className="links-wrapper">
                         <ul>
                             <li><a href="">Learn more</a></li>
                             <li><a href="">Order</a></li>
                         </ul>
                     </div>
-
                     <div className="ipod-caption row">
                         <div className="col-sm-12 col-md-6 text-md-right">iPad Pro available starting 3.25</div>
                         <div className="col-sm-12 col-md-6 text-md-left">Magic Keyboard coming in May</div>
                     </div>
                 </div>
             </section>
-            {/* First section ends here */}
 
-            {/* Second section starts here */}
+            {/* Second section */}
             <section className="second-hightlight-wrapper">
                 <div className="container">
-
-                    <div className="new-alert">
-                        New
-                    </div>
-
-                    <div className="title-wraper bold black">
-                        MacBook Air
-                    </div>
-
-                    <div className="description-wrapper black">
-                        Twice the speed. Twice the storage.
-                    </div>
-
-                    <div className="price-wrapper grey">
-                        From $999.
-                    </div>
-
+                    <div className="new-alert">New</div>
+                    <div className="title-wraper bold black">MacBook Air</div>
+                    <div className="description-wrapper black">Twice the speed. Twice the storage</div>
+                    <div className="price-wrapper grey">From $999.</div>
                     <div className="links-wrapper">
                         <ul>
                             <li><a href="">Learn more</a></li>
@@ -77,23 +58,13 @@ const Section = () => {
                     </div>
                 </div>
             </section>
-            {/* Second section ends here */}
 
-            {/* Third section starts here */}
+            {/* Third section */}
             <section className="third-hightlight-wrapper">
                 <div className="container">
-                    <div className="title-wraper bold">
-                        iPhone 11 Pro
-                    </div>
-
-                    <div className="description-wrapper">
-                        Pro cameras. Pro display. Pro performance.
-                    </div>
-
-                    <div className="price-wrapper">
-                        From $24.95/mo. or $599 with trade‑in.
-                    </div>
-
+                    <div className="title-wraper bold">iPhone 11 Pro</div>
+                    <div className="description-wrapper">Pro cameras. Pro display. Pro performance.</div>
+                    <div className="price-wrapper">From $24.95/mo. or $599 with trade‑in.</div>
                     <div className="links-wrapper">
                         <ul>
                             <li><a href="">Learn more</a></li>
@@ -102,24 +73,16 @@ const Section = () => {
                     </div>
                 </div>
             </section>
-            {/* Third section ends here */}
 
-            {/* Fourth section starts here */}
+            {/* Fourth section */}
             <section className="fourth-heghlight-wrapper">
                 <div className="container-fluid">
                     <div className="row">
                         <div className="left-side-wrapper col-sm-12 col-md-6">
                             <div className="left-side-container">
-                                <div className="title-wraper">
-                                    iPhone 11
-                                </div>
-                                <div className="description-wraper">
-                                    Just the right amount of everything.
-                                </div>
-                                <div className="price-wrapper">
-                                    From $18.70/mo. or $499 with trade‑in.<sup>1</sup>
-                                </div>
-
+                                <div className="title-wraper">iPhone 11</div>
+                                <div className="description-wraper">Just the right amount of everything.</div>
+                                <div className="price-wrapper">From $18.70/mo. or $499 with trade‑in.<sup>1</sup></div>
                                 <div className="links-wrapper">
                                     <ul>
                                         <li><a href="">Learn more</a></li>
@@ -130,10 +93,7 @@ const Section = () => {
                         </div>
                         <div className="right-side-wrapper col-sm-12 col-md-6">
                             <div className="right-side-container">
-                                <div className="title-wraper white">
-                                    Get the latest CDC response to COVID-19.
-                                </div>
-
+                                <div className="title-wraper white">Get the latest CDC response to COVID-19.</div>
                                 <div className="links-wrapper white">
                                     <ul>
                                         <li><a href="">Watch the PSA</a></li>
@@ -144,24 +104,17 @@ const Section = () => {
                     </div>
                 </div>
             </section>
-            {/* Fourth section ends here */}
 
-            {/* Fifth section starts here */}
+            {/* Fifth section */}
             <section className="fifth-heghlight-wrapper">
                 <div className="container-fluid">
                     <div className="row">
                         <div className="left-side-wrapper col-sm-12 col-md-6">
                             <div className="left-side-container">
                                 <div className="top-logo-wrapper">
-                                    <div className="logo-wrapper">
-                                        <img src={appleTv}/>
-                                    </div>
+                                    <div className="logo-wrapper"><img src={appleTv} /></div>
                                 </div>
-
-                                <div className="tvshow-logo-wraper">
-                                    <img src={banker}/>
-                                </div>
-
+                                <div className="tvshow-logo-wraper"><img src={banker} /></div>
                                 <div className="watch-more-wrapper">
                                     <a href="#">Watch now on the Apple TV App</a>
                                 </div>
@@ -170,13 +123,11 @@ const Section = () => {
                         <div className="right-side-wrapper col-sm-12 col-md-6">
                             <div className="right-side-container">
                                 <div className="top-logo-wrapper">
-                                    <div className="logo-wrapper">
-                                        <img src={watch}/>
-                                    </div>
+                                    <div className="logo-wrapper"><img src={watch} /></div>
                                 </div>
                                 <div className="description-wraper">
-                                    With the Always-On Retina display.<br/>
-                                        You’ve never seen a watch like this.
+                                    With the Always-On Retina display.<br />
+                                    You’ve never seen a watch like this.
                                 </div>
                                 <div className="links-wrapper">
                                     <ul>
@@ -189,18 +140,15 @@ const Section = () => {
                     </div>
                 </div>
             </section>
-            {/* Fifth section ends here */}
 
-            {/* Sixth section starts here */}
+            {/* Sixth section */}
             <section className="sixth-heghlight-wrapper">
                 <div className="container-fluid">
                     <div className="row">
                         <div className="left-side-wrapper col-sm-12 col-md-6">
                             <div className="left-side-container">
                                 <div className="top-logo-wrapper">
-                                    <div className="logo-wrapper">
-                                        <img src={arcade}/>
-                                    </div>
+                                    <div className="logo-wrapper"><img src={arcade} /></div>
                                 </div>
                                 <div className="description-wraper white">
                                     Agent 8 is a small hero on a big mission.
@@ -215,12 +163,8 @@ const Section = () => {
                         </div>
                         <div className="right-side-wrapper col-sm-12 col-md-6">
                             <div className="right-side-container">
-                                <div className="title-wraper">
-                                    Apple Card Monthly Installments
-                                </div>
-                                <div className="description-wraper">
-                                    Pay for your next iPhone over time, interest-free with Apple Card.
-                                </div>
+                                <div className="title-wraper">Apple Card Monthly Installments</div>
+                                <div className="description-wraper">Pay for your next iPhone over time, interest-free with Apple Card.</div>
                                 <div className="links-wrapper">
                                     <ul>
                                         <li><a href="">Learn more</a></li>
